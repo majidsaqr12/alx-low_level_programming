686b26d685e26238d7c5fcd11b3ab1af2f463e47
majidsaqr <majidsakr86@gmail.com>
2023-03-16 17:51:15 +0200

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types on the computer
+ * Return: 0 (Success)
+ */
+int main()
+{
+    int integerType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+    // Sizeof operator is used to evaluate the size of a variable
+    printf("Size of int: %ld bytes\n",sizeof(integerType));
+    printf("Size of float: %ld bytes\n",sizeof(floatType));
+    printf("Size of double: %ld bytes\n",sizeof(doubleType));
+    printf("Size of char: %ld byte\n",sizeof(charType));
+
+    return 0;
+}
