6d0ea99a94ad5928e9287b918dbb37c75cd5a13a
majidsaqr <majidsakr86@gmail.com>
2023-03-17 22:07:30 +0200

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int myNum;
+	scanf("%d", &myNum);
+	if(myNum > 0)
+	{
+		printf("%d is positive\n", n );
+	}
+	else if (myNum == 0)
+	{
+		printf("%d is zero\n", n );
+	}
+	else
+	{
+		printf("%d is negative\n", n );
+	}
+
+}
