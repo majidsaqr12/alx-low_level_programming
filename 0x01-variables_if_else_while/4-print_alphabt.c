553ceed9e577088d6fba4aa8e75a852ade616761
majidsaqr <majidsakr86@gmail.com>
2023-03-19 00:59:15 +0200

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-print_alphabt.c -o 4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,20 +7,16 @@
 
 int main(void)
 {
-        char c;
+        char c, e, q;
 
-        for (c = 'a'; c <= 'z'; c++)
-        {
-            if (c == 'e' || c == 'q')
-            {
-                
-            }
-            else
-            {
-                putchar(c);
-                putchar('\n');
-            }
-        }
+	e = 'e';
+	q = 'q';
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	if (c !=e && c != q)
+	putchar(c);
+	}
+	putchar('\n');
         return (0);
 }
