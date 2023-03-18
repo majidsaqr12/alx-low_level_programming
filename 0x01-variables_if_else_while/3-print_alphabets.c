7d078bf1be72f11d77e560c42c123d5aa255ef4a
majidsaqr <majidsakr86@gmail.com>
2023-03-19 00:27:21 +0200

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints the alphabet in lowercase, and then in uppercase
- * Return: Always (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
+
 	char c;
 
 	for(c = 'a'; c <= 'z'; c++)
@@ -15,5 +17,7 @@ int main(void)
 	for(c = 'A'; c <= 'Z'; c++)
 	putchar(c);
 	putchar('\n');
+
 	return (0);
+
 }
